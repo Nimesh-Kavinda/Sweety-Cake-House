cd7317dc565b869f0c954946a4939a415a23c3b9
Lakshika999 <madumalithilakarathna11@gmail.com>
2024-11-06 16:01:54 -0800

    create login and registration

--- index.php
+++ index.php
@@ -1,3 +1,86 @@
+<?php
+include './includes/db.php';
+?>
+<!-- registraion function  -->
+
+<?php
+
+@include '../includes/db.php';
+
+if(isset($_POST['submit'])){
+
+   $filter_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+   $name = mysqli_real_escape_string($conn, $filter_name);
+   $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+   $email = mysqli_real_escape_string($conn, $filter_email);
+   $filter_pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
+   $pass = mysqli_real_escape_string($conn, md5($filter_pass));
+   $filter_cpass = filter_var($_POST['cpass'], FILTER_SANITIZE_STRING);
+   $cpass = mysqli_real_escape_string($conn, md5($filter_cpass));
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
+
+   if(mysqli_num_rows($select_users) > 0){
+      $message[] = 'user already exist!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'confirm password not matched!';
+      }else{
+         mysqli_query($conn, "INSERT INTO `users`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
+         $message2[] = 'registered successfully!';
+      }
+   }
+
+}
+
+?>
+
+<!-- login fun  -->
+
+<?php
+
+
+session_start();
+
+if(isset($_POST['login_submit'])){
+
+   $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+   $email = mysqli_real_escape_string($conn, $filter_email);
+   $filter_pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
+   $pass = mysqli_real_escape_string($conn, md5($filter_pass));
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+
+   if(mysqli_num_rows($select_users) > 0){
+      
+      $row = mysqli_fetch_assoc($select_users);
+
+      if($row['user_type'] == 'admin'){
+
+         $_SESSION['admin_name'] = $row['name'];
+         $_SESSION['admin_email'] = $row['email'];
+         $_SESSION['admin_id'] = $row['id'];
+         header('location:./admin/admin_page.php');
+
+      }elseif($row['user_type'] == 'user'){
+
+         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['user_email'] = $row['email'];
+         $_SESSION['user_id'] = $row['id']; 
+         header('location:./home.php');
+
+      }else{
+         $message3[] = 'no user found!';
+      }
+
+   }else{
+      $message3[] = 'incorrect email or password!';
+   }
+
+}
+ 
+?>
 
 <!doctype html>
  <html lang="en" class="data-bs-theme">
