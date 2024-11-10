e34e0c5c7464a55f918e17f2e2c9e47c615788f6
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2024-11-10 09:42:32 +0530

    Create a Password Updating Function for User

--- includes/user_profile.php
+++ includes/user_profile.php
@@ -1,3 +1,28 @@
+<?php
+
+@include '../includes/db.php';
+
+
+$user_id = $_SESSION['user_id'];
+
+if (isset($_POST['update_password'])) {
+    $current_pass = md5(mysqli_real_escape_string($conn, $_POST['current_pass']));
+    $new_pass = md5(mysqli_real_escape_string($conn, $_POST['new_pass']));
+    $confirm_new_pass = md5(mysqli_real_escape_string($conn, $_POST['confirm_new_pass']));
+
+    $check_current_pass = mysqli_query($conn, "SELECT password FROM `users` WHERE id = '$user_id' AND password = '$current_pass'") or die('query failed');
+    
+    if (mysqli_num_rows($check_current_pass) === 0) {
+        $message[] = 'Current password is incorrect!';
+    } elseif ($new_pass != $confirm_new_pass) {
+        $message[] = 'New passwords do not match!';
+    } else {
+        mysqli_query($conn, "UPDATE `users` SET password = '$new_pass' WHERE id = '$user_id'") or die('query failed');
+        $message[] = 'Password updated successfully!';
+    }
+}
+?>
+
 <section class="user_profile">
    
     <div class="offcanvas offcanvas-end profile" tabindex="-1" id="offcanvasProfile" aria-labelledby="offcanvasProfileLabel">
@@ -18,10 +43,14 @@
         <ul class="list-group mb-4 mt-3">
           <li class="list-group-item"><strong>Email:</strong> <?php echo $_SESSION['user_email']; ?></li>
 
-          <!-- <form action="" class="list-group">
-          <input type="password" class="list-group-item" placeholder="Change Your Password">
-          <input type="submit" class="btn btn-outline-primary mt-3" value="Change Password">
-        </form> -->
+          <form action="" method="post" class="list-group mt-4">
+        <h6>Change Your Password</h6>
+        <input type="password" name="current_pass" class="list-group-item mb-2" placeholder="Enter Current Password" required>
+        <input type="password" name="new_pass" class="list-group-item mb-2" placeholder="Enter New Password" required>
+       <input type="password" name="confirm_new_pass" class="list-group-item mb-2" placeholder="Confirm New Password" required>
+       <input type="submit" name="update_password" class="btn btn-outline-primary mt-3" value="Change Password">
+     </form>
+
 
         </ul>
   
